8315afb332bb5e3e92bd432644ad087d0610d04d
Ozodbek <ozodbekozodov0002@gmail.com>
2022-09-22 14:26:06 +0500

    🌪🌪🌪

--- connect.php
+++ connect.php
@@ -1,9 +1,8 @@
 <?php
-//mysql://b9953d575bd1d4:52c1bcc5@us-cdbr-east-06.cleardb.net/heroku_1d7209728fd0f15?reconnect=true
 $servername = "us-cdbr-east-06.cleardb.net";
-$username = "b9953d575bd1d4";
-$password = "52c1bcc5";
-$db="heroku_1d7209728fd0f15";
+$username = "bf2b4db78430a0";
+$password = "ef420036";
+$db="heroku_15f16b4c96fc55b";
 $conn = mysqli_connect("$servername", "$username", "$password","$db");
 mysqli_set_charset($conn,'utf8');
 
